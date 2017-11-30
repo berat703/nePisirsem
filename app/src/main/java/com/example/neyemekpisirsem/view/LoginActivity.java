a7c7213759e3e1a655c12b2500eac3c2a06dc683
Berat Nakip <beratnakip@gmail.com>
2017-11-30 16:29:05 +0000

    login ve register yapıldı

--- app/src/main/java/com/example/neyemekpisirsem/view/LoginActivity.java
+++ app/src/main/java/com/example/neyemekpisirsem/view/LoginActivity.java
@@ -1,249 +1,212 @@
 package com.example.neyemekpisirsem.view;
 
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.neyemekpisirsem.R;
 import com.example.neyemekpisirsem.model.Users;
-import com.example.neyemekpisirsem.presenter.login_presenter;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.MobileServiceList;
 import com.microsoft.windowsazure.mobileservices.http.NextServiceFilterCallback;
-import com.microsoft.windowsazure.mobileservices.http.OkHttpClientFactory;
 import com.microsoft.windowsazure.mobileservices.http.ServiceFilter;
 import com.microsoft.windowsazure.mobileservices.http.ServiceFilterRequest;
 import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
 import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
-import com.microsoft.windowsazure.mobileservices.table.sync.MobileServiceSyncContext;
-import com.microsoft.windowsazure.mobileservices.table.sync.localstore.ColumnDataType;
-import com.microsoft.windowsazure.mobileservices.table.sync.localstore.MobileServiceLocalStoreException;
-import com.microsoft.windowsazure.mobileservices.table.sync.localstore.SQLiteLocalStore;
-import com.microsoft.windowsazure.mobileservices.table.sync.synchandler.SimpleSyncHandler;
-import com.squareup.okhttp.OkHttpClient;
 
 import java.net.MalformedURLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Pınar Köroğlu on 24.11.2017.
  */
 
 public class LoginActivity extends Activity {
-
-
+    Button bt_login;
+    Button bt_cancel;
+    Button bt_forgot;
     private MobileServiceClient mClient;
+    private MobileServiceTable<Users> mUser;
 
-    private MobileServiceTable<Users> userTable;  //modelden
-
-    private login_presenter loginPresenter;
-
+    EditText lEmail;
+    EditText lPass;
     private ProgressBar mProgressBar;
-    EditText edit1;
-    EditText edit2;
-    EditText edit3;
-    //EditText edit4;
-    // EditText edit5;
-    Button register;
-   // View view = setContentView(R.layout.activity_login);
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        try {
 
+    String lt;
+    String lg;
+    int days;
+
+    String ver;
+
+
+    String User_id;
+
+    String mypass;
+    SharedPreferences myaccount ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        bt_login = (Button) findViewById(R.id.loginButton);
+        bt_cancel = (Button) findViewById(R.id.cancel);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+
+
+        try {
+            // Create the Mobile Service Client instance, using the provided
+            // Mobile Service URL and key
             mClient = new MobileServiceClient(
                     "https://neyemekpisirsem.azurewebsites.net",
-                    this).withFilter(new com.example.neyemekpisirsem.view.LoginActivity.ProgressFilter());
+                    this)/*.withFilter(new LoginActivity.ProgressFilter())*/;
 
-            mClient.setAndroidHttpClientFactory(new OkHttpClientFactory() {
-                @Override
-                public OkHttpClient createOkHttpClient() {
-                    OkHttpClient client = new OkHttpClient();
-                    client.setReadTimeout(20, TimeUnit.SECONDS);
-                    client.setWriteTimeout(20, TimeUnit.SECONDS);
-                    return client;
-                }
-            });
-            userTable = mClient.getTable(Users.class);
-            initLocalStore().get();
+            // Get the Mobile Service Table instance to use
+            mUser = mClient.getTable(Users.class);
 
-           /* loginView = new loginView(this, R.layout.row_list_to_do);
-            ListView listViewToDo = (ListView) findViewById(R.id.listViewToDo);
-            listViewToDo.setAdapter(loginView);
-*/
-            // Load the items from the Mobile Service
-            //  refreshItemsFromTable();
 
         } catch (MalformedURLException e) {
-            createAndShowDialog(new Exception("There was an error creating the Mobile Service. Verify the URL"), "Error");
-        } catch (Exception e) {
-            createAndShowDialog(e, "Error");
+            e.printStackTrace();
         }
 
-    }
+        lEmail = (EditText) findViewById(R.id.t_username);
+        lPass = (EditText) findViewById(R.id.t_password);
 
 
 
-    @Override
-    public boolean onOptionsItemSelected (MenuItem item){
-        if (item.getItemId() == R.id.menu_refresh) {
-            // refreshItemsFromTable();
-        }
-
-        return true;
-    }
-
-
-    public void addItem(View view) {
-        if (mClient == null) {
-            return;
-        }
-
-        // Create a new item
-        final Users users = new Users();
-        edit1=(EditText)findViewById(R.id.usernameEditText);
-        edit2=(EditText)findViewById(R.id.passwordEditText);
-        edit3=(EditText)findViewById(R.id.emailEditText);
-       // edit4=(EditText)findViewById(R.id.editText4);
-        // edit5=(EditText)findViewById(R.id.editText5);
-        register=(Button)findViewById(R.id.registerButton);
-
-
-        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
+        bt_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected Void doInBackground(Void... params) {
-                return null;
-            }
-        };
-
-        runAsyncTask(task);
-
-        edit1.setText("");
-    }
-
-    public Users addItemInTable(Users user) throws ExecutionException, InterruptedException {
-        Users entity = userTable.insert(user).get();
-        return entity;
-    }
-   /* private void refreshItemsFromTable() {
-
-
-        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
-            @Override
-            protected Void doInBackground(Void... params) {
-
-                try {
-                    final List<Users> results = refreshItemsFromMobileServiceTable();
-
-                    //Offline Sync
-                    //final List<ToDoItem> results = refreshItemsFromMobileServiceTableSyncTable();
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            loginPresenter.clear();
-
-                            for (Users user : results) {
-                                loginView.add(user);
-                            }
-                        }
-                    });
-                } catch (final Exception e){
-                    createAndShowDialogFromTask(e, "Error");
-                }
-
-                return null;
-            }
-        };
-
-        runAsyncTask(task);
-    }*/
-
-    private AsyncTask<Void, Void, Void> initLocalStore() throws MobileServiceLocalStoreException, ExecutionException, InterruptedException {
-
-        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                try {
-
-                    MobileServiceSyncContext syncContext = mClient.getSyncContext();
-
-                    if (syncContext.isInitialized())
-                        return null;
-
-                    SQLiteLocalStore localStore = new SQLiteLocalStore(mClient.getContext(), "OfflineStore", null, 1);
-
-                    Map<String, ColumnDataType> tableDefinition = new HashMap<String, ColumnDataType>();
-                    tableDefinition.put("name", ColumnDataType.String);
-                    tableDefinition.put("email", ColumnDataType.String);
-                    tableDefinition.put("username", ColumnDataType.Boolean);
-                    tableDefinition.put("password", ColumnDataType.Boolean);
-                    localStore.defineTable("Users", tableDefinition);
-
-                    SimpleSyncHandler handler = new SimpleSyncHandler();
-
-                    syncContext.initialize(localStore, handler).get();
-
-                } catch (final Exception e) {
-                    createAndShowDialogFromTask(e, "Error");
-                }
-
-                return null;
-            }
-        };
-
-        return runAsyncTask(task);
-    }
-
-    private void createAndShowDialogFromTask(final Exception exception, String title) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                createAndShowDialog(exception, "Error");
+            public void onClick(View view) {
+                Intent fp = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(fp);
             }
         });
     }
 
-    private void createAndShowDialog(Exception exception, String title) {
-        Throwable ex = exception;
-        if(exception.getCause() != null){
-            ex = exception.getCause();
+    public void add(View view) {
+
+        if (lEmail.getText().toString().trim().equals("")) {
+            lEmail.setError("Email is required!");
+            mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+            return;
+            //You can Toast a message here that the Username is Empty
         }
-        createAndShowDialog(ex.getMessage(), title);
-    }
 
-    private void createAndShowDialog(final String message, final String title) {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setMessage(message);
-        builder.setTitle(title);
-        builder.create().show();
-    }
-
-    private AsyncTask<Void, Void, Void> runAsyncTask(AsyncTask<Void, Void, Void> task) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            return task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-        } else {
-            return task.execute();
+        if (lPass.getText().toString().trim().equals("")) {
+            lPass.setError("Password is required!");
+            mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+            return;
+            //You can Toast a message here that the Username is Empty
         }
+
+
+        mProgressBar.setVisibility(ProgressBar.VISIBLE);
+
+
+        final String mail;
+        final String pwd;
+
+        mail = lEmail.getText().toString();
+        pwd = lPass.getText().toString();
+
+
+        new AsyncTask<Void, Void, Void>() {
+
+            Boolean validation = false;
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                Log.d("try", "do in background");
+                try {
+                    final MobileServiceList<Users> result =
+                        //    mUser.select("email").execute().get();
+                            mUser.where().field("email").eq(mail).and(mUser.where().field("password").eq(pwd)).execute().get();
+                             Log.d("tag","asd"+result);
+
+
+                    Log.d("try", "got the result");
+
+
+                    for (Users item : result) {
+
+                        validation = item.getPassword().toString().equalsIgnoreCase(pwd);
+
+
+                        Log.d("try", "" + item);
+
+
+                        User_id = item.getId().toString();  // you can get columns data like this
+
+
+                        runOnUiThread(new Runnable() {
+
+                            public void run() {
+
+
+                                Toast.makeText(getApplicationContext(), "SUCCESS", Toast.LENGTH_SHORT).show();
+                                Intent MainActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                                startActivity(MainActivity);
+
+
+                            }
+
+
+                        });
+
+
+                    }
+
+
+                    if (validation == false) {
+
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                Toast toast = Toast.makeText(getApplicationContext(), "Email or Password is Wrong", Toast.LENGTH_LONG);
+                                toast.setGravity(Gravity.CENTER, 0, 0);
+                                toast.show();
+                                mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+                            }
+                        });
+
+
+                    }
+
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                    Log.d("Error", "catching the error");
+                }
+                return null;
+            }
+
+        }.execute();
     }
 
 
+    //   @Override
+  /*  public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
 
     private class ProgressFilter implements ServiceFilter {
 
@@ -260,6 +223,7 @@ public class LoginActivity extends Activity {
             return resultFuture;
         }
     }
+
 }
 
 
