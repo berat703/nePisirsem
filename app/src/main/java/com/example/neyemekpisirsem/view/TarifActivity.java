48a527462fc5c897c79b59acbf079ffba280e86c
DemetSancakli <demet.sancakli@hotmail.com>
2017-12-11 02:12:32 +0300

    Tarif Sayfası

--- app/src/main/java/com/example/neyemekpisirsem/view/TarifActivity.java
+++ app/src/main/java/com/example/neyemekpisirsem/view/TarifActivity.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 
 public class TarifActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tarif);
-    }
+    //@Override
+   // protected void onCreate(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_tarif);
+    //}
 }
