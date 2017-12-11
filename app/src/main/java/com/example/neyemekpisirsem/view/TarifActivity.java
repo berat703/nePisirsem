ffd5f4341cd463905292ba8499ba45c671cb38e3
DemetSancakli <demet.sancakli@hotmail.com>
2017-12-11 22:32:32 +0300

    Tarif sayfası son hal

--- app/src/main/java/com/example/neyemekpisirsem/view/TarifActivity.java
+++ app/src/main/java/com/example/neyemekpisirsem/view/TarifActivity.java
@@ -1,13 +1,22 @@
 package com.example.neyemekpisirsem.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.neyemekpisirsem.R;
 
 public class TarifActivity extends AppCompatActivity {
 
-    //@Override
-   // protected void onCreate(Bundle savedInstanceState) {
-        //super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_tarif);
-    //}
+    @Override
+   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarif);
+    }
+
+    public void onceki(View view) {
+        Intent i = new Intent(getApplicationContext(),FoodActivity.class);
+        startActivity(i);
+    }
 }
